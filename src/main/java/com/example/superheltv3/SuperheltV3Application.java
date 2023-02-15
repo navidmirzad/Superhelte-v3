52719bddeeb5c632922f5dbb1810c9512d11e5fb
navidmirzad <navi0221@stud.kea.dk>
2023-02-15 08:51:42 +0100

    First commit

---
+++ src/main/java/com/example/superheltv3/SuperheltV3Application.java
@@ -0,0 +1,13 @@
+package com.example.superheltv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheltV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperheltV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/superheltv3/SuperheltV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superheltv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheltV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
